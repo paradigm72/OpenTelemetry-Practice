ca4d300452a67715f8387a254ba824bd60a0eb5d
Paul Romine <promine@gmail.com>
2025-01-11 07:56:25 -0600

    richer metrics and exceptions
    add a couple layers of interesting data for later visualizing

--- venv/app.py
+++ venv/app.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 from opentelemetry import trace
 from opentelemetry import metrics
 import logging
+import json
 
 # Acquire a tracer
 tracer = trace.get_tracer("diceroller.tracer")
@@ -26,27 +27,44 @@ def roll_dice():
     player = request.args.get('player', default=None, type=str)
     result = str(roll())
     if player:
-        pass
-        #logger.warning("%s is rolling the dice: %s", player, result)
+        logger.warning("%s is rolling the dice: %s", player, result)
     else:
-        pass
-        #logger.warning("Anonymous player is rolling the dice: %s", result)
+        logger.warning("Anonymous player is rolling the dice: %s", result)
     return result
 
 
 def roll():
     # This creates a new span that's the child of the current one
     with tracer.start_as_current_span("roll") as rollspan:
-        res = randint(1, 20)
+        hasAdvantage = randint(0, 2)
+        if hasAdvantage == 2:  # one-third of the time
+            res = roll1 = roll2 = randint(1, 20)
+            rawRollsDict = { "res": res}
+            
+        else:
+            roll1 = randint(1, 20)
+            roll2 = randint(1, 20)
+            res = max(roll1, roll2)
+            rawRollsDict = { "roll1": roll1, "roll2": roll2}
+            if (roll1 == roll2):
+                rollspan.set_attribute("roll.tag", "Both rolls were the same; isn't that interesting?")
+
+        # store the raw rolls, which could be 1 or 2 values in a serialized dictionary
+        rawRolls = json.dumps(rawRollsDict)
+        rollspan.set_attribute("roll.rawRolls", rawRolls)
+
         rollspan.set_attribute("roll.value", res)
         rollspan.set_attribute("roll.crit", 1 if res == 20 else 0)
+        rollspan.set_attribute("roll.rolledWithAdvantage", hasAdvantage)
         roll_counter.add(1, {"roll.value": res})
 
-        if (res == 1):
-            try:
+        try:
+            if (hasAdvantage and ((roll1 == 1)  or (roll2 == 1))):
+                raise AssertionError("The player rolled a 1, but thankfully, it didn't count")
+            elif (res == 1):
                 raise AssertionError("The player rolled a 1!")
-            except AssertionError as e:
-                rollspan.record_exception(e, attributes={"roll.value": 1})
-                rollspan.set_status(trace.status.Status(trace.status.StatusCode.ERROR, str(e)))
+        except AssertionError as e:
+            rollspan.record_exception(e, attributes={"roll.value": 1})
+            rollspan.set_status(trace.status.Status(trace.status.StatusCode.ERROR, str(e)))
 
-        return res
+        return f"{roll1}, {roll2} = {res}" if hasAdvantage else str(res)
